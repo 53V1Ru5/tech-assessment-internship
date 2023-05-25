ad0a318ed9172518d3a2be66287803ec6c8723da
Ivan <ikladinov@gmail.com>
2023-05-25 19:51:51 -0400

    updated code

--- assets/main.js
+++ assets/main.js
@@ -1,83 +1,24 @@
-// let submitForm = document.getElementById("submitForm");
 
-// submitForm.addEventListener("submit", (e) => {
-//   e.preventDefault();
+// Get references to the form and background elements
+const form = document.forms.submitForm;
+const container = document.querySelector('.container');
 
-//   let name = document.getElementById("name");
-//   let email = document.getElementById("email");
-//   let handle = document.getElementById("handle");
+// Function to handle form submission
+function validForm(event) {
+    // Prevent the form from submitting and page reloading
+    event.preventDefault();
 
-//   if (name.value == "" || email.value == "") {
-//     alert("Ensure you input a value in both fields!");
-//   } else {
-//     // perform operation with form input
-//     alert("This form has been successfully submitted!");
-//     console.log(
-//       `This form has a name of ${name.value}, email of ${email.value} and handle of ${handle.value}`
-//     );
+    // Remove the background image
+    container.style.backgroundImage = 'none';
 
-//     name.value = "";
-//     email.value = "";
-//     handle.value = "";
+    // Change the background color to your desired color
+    container.style.backgroundColor = '#FF0000'; // Example: changing to red color
 
-//   }
-// });
+    // Perform any additional actions or validations here
 
-// const form = document.getElementById("submitForm");
-
-// form.addEventListener("submit", onFormSubmit);
-
-// function onFormSubmit(event) {
-// 	event.preventDefault();
-// 	const data = new FormData(event.target);
-
-// 	const name = data.get("name");
-// 	const email = data.get("email");
-//     const handle = data.get("handle");
-// 	console.log(`Name: ${name}, Email: ${email}, Handle: ${handle}`);
-// }
-
-
-// const submitForm = document.getElementById("submitForm");
-
-// submitForm.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   console.log("Form has been submitted");
-// });
-
-
-
-function validForm() {
-  //alert("Alert!");
-  let name = document.submitForm.name;
-  let email = document.submitForm.email;
-  let handle = document.submitForm.handle;
-
-  if(name.value == "") {
-    name.nextElementSibling.style.display = "block";
-    name.style.border = "1px solid #f00";
-    return false;
-  } else {
-    name.nextElementSibling.style.display = "none";
-    name.style.border = "1px solid transparent";
-  }
-
-  if(email.value == "") {
-    email.nextElementSibling.style.display = "block";
-    email.style.border = "1px solid #f00";
-    return false;
-  } else {
-    email.nextElementSibling.style.display = "none";
-    email.style.border = "1px solid transparent";
-  }
-
-  if(handle.value == "") {
-    handle.nextElementSibling.style.display = "block";
-    handle.style.border = "1px solid #f00";
-    return false;
-  } else {
-    handle.nextElementSibling.style.display = "none";
-    handle.style.border = "1px solid transparent";
-  }
+    // Submit the form
+    form.submit();
 }
 
+// Add an event listener to the form's submit event
+form.addEventListener('submit', validForm);
